39388222a03b7b8ff04b8efffb45b43fa2cd23a0
Komil <K.juraev1994@gmail.com>
2022-05-04 16:55:05 -0400

    updated web to fit smaller screens

---
+++ build/precache-manifest.3c9bf97810141dd3ed319e2707a30dc1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c34a41950c352b41050",
+    "url": "/personal-web-app/static/js/main.0c34a419.chunk.js"
+  },
+  {
+    "revision": "b740295c38ff61db2abc",
+    "url": "/personal-web-app/static/js/1.b740295c.chunk.js"
+  },
+  {
+    "revision": "5c23387896f7e92c1abe",
+    "url": "/personal-web-app/static/js/runtime~main.5c233878.js"
+  },
+  {
+    "revision": "8e5c672806788fbfa3d27cad1d80b284",
+    "url": "/personal-web-app/index.html"
+  }
+];
